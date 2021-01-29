413686171f27e3bc85457b11e8a0422482eb2108
Lexingto <74386589+Lexingto@users.noreply.github.com>
2021-01-29 23:52:55 +0000

    Top Down RPG Game I made for my 1st Year Game Dev Course

---
+++ topdownrpg/Assets/Scripts/UI/helahtbarscript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class helahtbarscript : MonoBehaviour
+{
+    Image healthbar;
+    float maxHealth = 100f; //the max amount of health
+    public static float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    healthbar = GetComponent<Image>(); //displays the healthbar image
+    health = maxHealth; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    healthbar.fillAmount = health / maxHealth; //health divided by the max amount you can have
+    }
+}
